3caa2622b7f29495fc05bf1de231dceb3852dd4b
MaximMukhin <eriwer@gmail.com>
2022-10-02 19:13:44 +0300

    start guess the number

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// document.querySelector('.check').addEventListener('click', function (event) {
+//   console.log(event.target.textContent);
+// });
+
+console.log(document.querySelector('.check'));
