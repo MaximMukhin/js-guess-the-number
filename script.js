2d5f7cfc518c2b18df1a1bfc463c8820d4ba30d8
MaximMukhin <eriwer@gmail.com>
2022-10-04 22:36:29 +0300

    fix

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.question').textContent = secretNumber;
+console.log(secretNumber);
+
 document.querySelector('.check').addEventListener('click', function (event) {
-  const guessNumber = document.querySelector('.number-input').value;
+  const guessNumber = Number(document.querySelector('.number-input').value);
   console.log(guessNumber);
+
+  console.log(secretNumber, 'secretNumber');
+  console.log(guessNumber, 'guessNumber');
 
   if (!guessNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+    console.log('Правильно!');
   }
 });
