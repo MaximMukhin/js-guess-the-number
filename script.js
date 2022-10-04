7b91e00b55704a1d9fd8d2a0c31270c71d56c81c
MaximMukhin <eriwer@gmail.com>
2022-10-04 22:48:57 +0300

    guessNumber

--- script.js
+++ script.js
@@ -2,19 +2,17 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.question').textContent = secretNumber;
-console.log(secretNumber);
 
 document.querySelector('.check').addEventListener('click', function (event) {
   const guessNumber = Number(document.querySelector('.number-input').value);
-  console.log(guessNumber);
-
-  console.log(secretNumber, 'secretNumber');
-  console.log(guessNumber, 'guessNumber');
 
   if (!guessNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
   } else if (guessNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Правильно!';
-    console.log('Правильно!');
+  } else if (guessNumber > secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Меньше!';
+  } else if (guessNumber < secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Больше!';
   }
 });
