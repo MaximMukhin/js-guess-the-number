ae79faa8fe36401f68b83bbee5e99542c10bf8db
MaximMukhin <eriwer@gmail.com>
2022-10-05 21:42:29 +0300

    fix

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.question').textContent = secretNumber;
 
+let score = 20;
+document.querySelector('.score').textContent = score;
+
 document.querySelector('.check').addEventListener('click', function (event) {
   const guessNumber = Number(document.querySelector('.number-input').value);
 
@@ -10,9 +13,26 @@ document.querySelector('.check').addEventListener('click', function (event) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
   } else if (guessNumber === secretNumber) {
     document.querySelector('.guess-message').textContent = 'Правильно!';
+    document.querySelector('body').style.backgroundColor = 'green';
   } else if (guessNumber > secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Меньше!';
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Число меньше!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Проиграл!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
   } else if (guessNumber < secretNumber) {
-    document.querySelector('.guess-message').textContent = 'Больше!';
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Число больше!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Проиграл!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
   }
 });
