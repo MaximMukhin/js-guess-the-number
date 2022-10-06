7020718c0ed95aed580635237c16500c5b2b2c2e
MaximMukhin <eriwer@gmail.com>
2022-10-06 21:52:00 +0300

    fix

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const setNumberLarger = () => {
 const setHighScore = () => {
   highScore = score;
   highScoreElement.textContent = highScore;
-}
+};
 
 newGameElement.addEventListener('click', () => setNewGame());
 
